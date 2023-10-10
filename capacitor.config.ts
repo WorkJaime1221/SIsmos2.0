e1ad9b21b50432b40cd1874aee9072cfec23ed26
Jaime L <113290421+WorkJaime1221@users.noreply.github.com>
2023-10-09 22:37:53 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sismos3.0',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
